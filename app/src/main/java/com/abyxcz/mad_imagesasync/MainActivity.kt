80ac2ca6d5d095957ea0fbd4635a10fd841ef0f1
TM <tjmcard@gmail.com>
2024-01-03 12:07:27 -0800

    Added Coil image Composable, status images

--- app/src/main/java/com/abyxcz/mad_imagesasync/MainActivity.kt
+++ app/src/main/java/com/abyxcz/mad_imagesasync/MainActivity.kt
@@ -3,13 +3,18 @@ package com.abyxcz.mad_imagesasync
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.rememberAsyncImagePainter
 import com.abyxcz.mad_imagesasync.ui.theme.MADImagesAsyncTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,6 +32,18 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun DisplayImageFromUrl(imageUrl: String) {
+    Image(
+        painter = rememberAsyncImagePainter(model = imageUrl,
+            error = painterResource(R.drawable.error),
+            placeholder = painterResource(R.drawable.placeholder)),
+        contentDescription = "Loaded image",
+        modifier = Modifier.fillMaxWidth(),  // Or any modifier you require
+        contentScale = ContentScale.Crop  // Or any content scale you prefer
+    )
 }
 
 @Composable
